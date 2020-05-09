dcafaa34fadbbb4453bffd214f1d02c09db9cd77
Admin <admin@Suresh-Villuri-Macbook-Pro.local>
2020-05-09 08:18:08 -0700

    Fixed spacing in dashboard page

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Grid } from 'semantic-ui-react';
+import { Grid } from "semantic-ui-react";
 import TasksCompleted from "../../common/widgets/TasksCompleted";
 import LatestTasks from "../../common/widgets/LatestTasks";
 
@@ -8,30 +8,29 @@ import TasksChart from "../../common/widgets/TasksChart";
 import TasksList from "../TasksList";
 
 export default function Dashboard() {
-  return (<div className="dashboard-wrapper">
-    <Grid container
-      verticalAlign="middle"
-      justify="center"
-      style={{ minHeight: "100vh" }}>
-      <Grid.Row>
-        <Grid.Column width={5} stretched={true}>
-          <TasksCompleted />
-        </Grid.Column>
+	return (
+		<div className="dashboard-wrapper">
+			<Grid container verticalAlign="middle" justify="center">
+				<Grid.Row>
+					<Grid.Column width={5} stretched={true}>
+						<TasksCompleted />
+					</Grid.Column>
 
-        <Grid.Column width={5} stretched={true}>
-          <LatestTasks />
-        </Grid.Column>
+					<Grid.Column width={5} stretched={true}>
+						<LatestTasks />
+					</Grid.Column>
 
-        <Grid.Column width={5} stretched={true}>
-          <TasksChart />
-        </Grid.Column>
-      </Grid.Row>
+					<Grid.Column width={5} stretched={true}>
+						<TasksChart />
+					</Grid.Column>
+				</Grid.Row>
 
-      <Grid.Row>
-        <Grid.Column verticalAlign="top" width={15}>
-          <TasksList />
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
-  </div >)
+				<Grid.Row>
+					<Grid.Column verticalAlign="top" width={15}>
+						<TasksList />
+					</Grid.Column>
+				</Grid.Row>
+			</Grid>
+		</div>
+	);
 }
